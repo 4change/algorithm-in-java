65867514172354cca4b06b696f37b5d2252ddb1c
4change <2289200570@qq.com>
2023-05-16 15:41:27 +0800

    细节完善

--- src/main/java/com/java/algorithm/other/backpack/_0_1_backpack/Solution.java
+++ src/main/java/com/java/algorithm/other/backpack/_0_1_backpack/Solution.java
@@ -1,12 +1,21 @@
-package com.java.algorithm.other._0_1_backpack;
+package com.java.algorithm.other.backpack._0_1_backpack;
 
 public class Solution {
-	
+
 	public static void main(String[] args) {
 		System.out.println(backpack(4, 3, new int[] {2, 1, 3}, new int[] {4, 2, 3}));
-		System.out.println(backpack(5, 3, new int[] {2, 1, 3}, new int[] {4, 2, 3}));
+		System.out.println(backpack(11, 5, new int[] {1, 2, 5, 6, 7}, new int[] {1, 6, 18, 22, 28}));
 	}
-	
+
+	/**
+	 * 0-1 背包问题
+	 *
+	 * @param W		背包容量
+	 * @param N		物品总量
+	 * @param wt	物品总量数组
+	 * @param val	物品价值数组
+	 * @return
+	 */
 	public static int backpack(int W, int N, int[] wt, int[] val) {
 	    int[][] dp = new int[N+1][W+1];
 
@@ -26,7 +35,7 @@ public class Solution {
 //	    	for (int j = 0; j <= W; j++) {
 //	    		System.out.print(dp[i][j] + " ");
 //	    	}
-//	    	
+//
 //	    	System.out.println();
 //	    }
 
